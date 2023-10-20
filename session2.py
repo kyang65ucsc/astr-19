f332ce5efaddb046508922a37fd6fd5e8cf43e74
kyang65ucsc <146886773+kyang65ucsc@users.noreply.github.com>
2023-10-20 15:54:35 -0700

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,14 @@
+x = 2.0
+y = 2.0
+print('x = 2')
+print('y = 2')
+print('x + y =',x+y)
+print(type(x+y))
+x = int(x)
+y = int(y)
+print('x - y =',x-y)
+print(type(x-y))
+x = float(x)
+print('x * y =',x*y)
+print(type(x*y))
+
