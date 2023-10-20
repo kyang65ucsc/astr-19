95e14e17e1e7c44b60fff0ab0760f935e72caaa0
kyang65ucsc <146886773+kyang65ucsc@users.noreply.github.com>
2023-10-20 14:21:09 -0700

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,16 @@
+def PrintName():
+	print("Kenny Yang")
+
+def FavFood():
+	print("My favorite thing to eat is instant ramen.")
+
+def FavMovie():
+	print('My favorite movie of all time is Fast Five.')
+
+def main():
+	PrintName()
+	FavMovie()
+	FavFood()
+
+if __name__=="__main__":
+	main()
